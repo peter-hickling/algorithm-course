195798c95f496c5e2c97f729e7468e3ffc101610
Peter Hickling <peter.hickling@codera.co.uk>
2018-02-28 09:57:14 +0000

    Improved karatsuba multiplication.

--- algorithms/multiplication/karatsuba-multiplication.js
+++ algorithms/multiplication/karatsuba-multiplication.js
@@ -1,16 +1,17 @@
 const karatsubaMultiplication = (x, y) => {
-  let xStr = "" + x;
-  let yStr = "" + y;
-  let n = xStr.length;
-
+  let n = ("" + x).length;
   if (n <= 1) {
-    return xStr * yStr;
+    return x * y;
   }
 
-  let a = Number(xStr.substr(0, Math.floor(n / 2)));
-  let b = Number(xStr.substr(Math.ceil(n / 2), n));
-  let c = Number(yStr.substr(0, Math.floor(n / 2)));
-  let d = Number(yStr.substr(Math.ceil(n / 2), n));
+  let nHigh = Math.ceil(n/2);
+  let nlow = Math.floor(n/2);
+
+  let a = parseInt(x / Math.pow(10, nlow));
+  let b = x % Math.pow(10, nHigh);
+  let c = parseInt(y / Math.pow(10, nlow));
+  let d = y % Math.pow(10, nHigh);
+
   let aC = karatsubaMultiplication(a, c);
   let bD = karatsubaMultiplication(b, d);
 
@@ -18,4 +19,4 @@ const karatsubaMultiplication = (x, y) => {
   return aC * 10 ** n + (karatsubaMultiplication(a + b, c + d) - aC - bD) * 10 ** (n / 2) + bD;
 };
 
-console.log(karatsubaMultiplication(15, 13));
+console.log(karatsubaMultiplication(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627));
